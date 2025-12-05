d55560dbd86f783d50296c28b76f4a077e8ee451
sblakely01 <stephanye.blakely@gmail.com>
2025-12-05 14:42:08 -0500

    Made NavBar links white

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,10 @@
 import { NavLink } from "react-router-dom";
 function Navbar() {
     return (
-        <nav>
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/projects'>Projects</NavLink>
-        </nav>
+    <nav className="text-white flex justify-between items-center w-full h-10">
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/projects">Projects</NavLink>
+    </nav>
     )
 }
 export default Navbar
